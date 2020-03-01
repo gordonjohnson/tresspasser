e1c1883ea2e5fc1999b73e979b875736f84790c2
Gordon Johnson <gordonjohnson@outlook.com>
2020-02-29 16:58:39 -0800

    skip attaching audio to the document

--- src/sounds/soundEffects.ts
+++ src/sounds/soundEffects.ts
@@ -1,28 +1,14 @@
-const soundEffect = (src: string, volume = 1, loop = false) => {
-  // preload the audio file
-  let audioElement = document.createElement("audio");
-  audioElement.src = src;
-  audioElement.preload = "auto";
-  audioElement.volume = volume;
-  audioElement.loop = loop;
-  document.body.appendChild(audioElement);
-  return audioElement;
-};
+export const startStage = new Audio(require("./start-stage.mp3"));
+export const ringSelected = new Audio(require("./ring-selected.mp3"));
+export const portPowered = new Audio(require("./port-powered.mp3"));
+export const finalPortPowered = new Audio(require("./final-port-powered.mp3"));
 
-export const electricalHum = soundEffect(
-  require("./background-electrical-hum.mp3"),
-  0.25,
-  true
-);
+export const backgroundMusic = new Audio(require("./bg-music.mp3"));
+backgroundMusic.volume = 0.5;
+backgroundMusic.loop = true;
 
-export const finalPortPowered = soundEffect(
-  require("./final-port-powered.mp3")
+export const electricalHum = new Audio(
+  require("./background-electrical-hum.mp3")
 );
-export const portPowered = soundEffect(require("./port-powered.mp3"));
-export const ringSelected = soundEffect(require("./ring-selected.mp3"));
-export const startStage = soundEffect(require("./start-stage.mp3"));
-export const backgroundMusic = soundEffect(
-  require("./bg-music.mp3"),
-  0.5,
-  true
-);
+electricalHum.volume = 0.25;
+electricalHum.loop = true;
