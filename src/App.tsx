7cfbea339c7d3105625814e2b323c80dc404cbc2
Gordon Johnson <gordonjohnson@outlook.com>
2020-03-29 17:32:58 -0700

    add some basic instructions

--- src/App.tsx
+++ src/App.tsx
@@ -13,37 +13,52 @@ import "./App.css";
 function DebugStageSelector() {
   const { currentStage, setCurrentStage, goToTitleScreen } = useGameContext();
   return (
-    <label
+    <div
       style={{
         color: "white",
         fontSize: 16,
-        float: "right",
-        marginRight: 50,
         position: "fixed",
         fontFamily: "Aguda",
         zIndex: 1
       }}
     >
-      Stage Select{" "}
-      <select
-        onChange={e => {
-          if (e.currentTarget.value === "menu") {
-            goToTitleScreen();
-          } else {
-            setCurrentStage(parseInt(e.currentTarget.value, 10));
-          }
-        }}
-        style={{ fontSize: "inherit" }}
-        value={currentStage ?? undefined}
-      >
-        <option value={undefined}>menu</option>
-        {STAGES.map((s, idx) => (
-          <option key={idx} value={idx}>
-            {idx} - {s.name}
-          </option>
-        ))}
-      </select>
-    </label>
+      <label>
+        Stage Select{" "}
+        <select
+          onChange={e => {
+            if (e.currentTarget.value === "menu") {
+              goToTitleScreen();
+            } else {
+              setCurrentStage(parseInt(e.currentTarget.value, 10));
+            }
+          }}
+          style={{ fontSize: "inherit" }}
+          value={currentStage ?? undefined}
+        >
+          <option value={undefined}>menu</option>
+          {STAGES.map((s, idx) => (
+            <option key={idx} value={idx}>
+              {idx} - {s.name}
+            </option>
+          ))}
+        </select>
+      </label>
+      <p hidden={currentStage === null}>
+        Line up the laser beams with the ports
+        <br />
+        <small>
+          W = next exterior ring
+          <br />
+          S = next interior ring
+          <br />
+          A = rotate counter-clockwise
+          <br />
+          D = rotate clockwise
+          <br />
+          Spacebar = toggle ring power
+        </small>
+      </p>
+    </div>
   );
 }
 
